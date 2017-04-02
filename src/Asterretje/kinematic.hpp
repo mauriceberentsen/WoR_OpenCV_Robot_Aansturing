3c7851a0f75af8de7a67850a0c3cae3b308b4c0d
Maurice Berentsen <mauriceberentsen@live.nl>
2017-04-02 15:37:57 +0200

    added knimatic function 4dof jacobi

--- src/Asterretje/kinematic.hpp
+++ src/Asterretje/kinematic.hpp
@@ -33,6 +33,29 @@ struct Configuration
 
 };
 
+Matrix<4,3,double> jacobi_4dof(double phi0,double phi1,double phi2,double phi3,double phi4,double l1,double l2,double l3)
+{
+	/**afgeleiden**/
+	double dx0 = -l1 * cos(phi1) * sin(phi0) -l2 * cos(phi1 + phi2) * sin(phi0) -l3* cos(phi1 + phi2 + phi3) * sin(phi0);
+	double dx1 = -l1 * sin(phi1) * cos(phi0) -l2 * sin(phi1 + phi2) * cos(phi0) -l3* sin(phi1 + phi2 + phi3) * cos(phi0);
+	double dx2 = -l2 * sin(phi1 + phi2) * cos(phi0) -l3 * sin(phi1 + phi2 + phi3) * cos(phi0);
+	double dx3 = -l3 * sin(phi1 + phi2 + phi3) * cos(phi0);
+	double dy0 = -l1 * cos(phi1) * cos(phi0) -l2 * cos(phi1 + phi2) * cos(phi0) -l3* cos(phi1 + phi2 + phi3) * cos(phi0);
+	double dy1 = -l1 * sin(phi1) * sin(phi0) -l2 * sin(phi1 + phi2) * sin(phi0) -l3* sin(phi1 + phi2 + phi3) * sin(phi0);
+	double dy2 = -l2 * sin(phi1 + phi2) * sin(phi0) -l3* sin(phi1 + phi2 + phi3) * sin(phi0);
+	double dy3 = -l3* sin(phi1 + phi2 + phi3) * sin(phi0);
+	double dz0 = 0;
+	double dz1 = l1 * cos(phi1) + l2 * cos(phi1 + phi2) + l3 * cos(phi1 + phi2 + phi3);
+	double dz2 = l2 * cos(phi1 + phi2) + l3 * cos(phi1 + phi2 + phi3);
+	double dz3 = l3 * cos(phi1 + phi2 + phi3);
+	//jacobi
+	Matrix<4,3,double> mat = Matrix<4,3,double> ({{dx0,dx1,dx2,dx3},
+												  {dy0,dy1,dy2,dy3},
+												  {dz0,dz1,dz2,dz3}});
+	return mat;
+}
+
+
 std::pair<double,double> calculate_postion(const std::pair<double,double>& startpos, double lengte1,double phi1,double lengte2, double phi2, double lengte3, double phi3)
 {
   std::pair<double,double> position;
@@ -48,16 +71,19 @@ std::pair<double,double> calculate_postion(const std::pair<double,double>& start
 Point calculate_postition(const Configuration& myConfig)
 {
   Point currentPosition;
-  currentPosition.x = myConfig.currentPos.x +  (myConfig.length1 * cos(myConfig.phi1 * PI/180) * cos(myConfig.phi0 * PI/180))
+  currentPosition.x = myConfig.currentPos.x
+		  	  	  	  	+  (myConfig.length1 * cos(myConfig.phi1 * PI/180) * cos(myConfig.phi0 * PI/180))
                         +  (myConfig.length2 * cos((myConfig.phi1 + myConfig.phi2) * PI/180)) * cos(myConfig.phi0 * PI/180)
                         +  (myConfig.length3 * cos((myConfig.phi1 + myConfig.phi2 + myConfig.phi3) * PI/180)) * cos(myConfig.phi0 * PI/180);
 
-  currentPosition.y = myConfig.currentPos.y +  (myConfig.length1 * sin(myConfig.phi1 * PI/180) * sin(myConfig.phi0 * PI/180))
+  currentPosition.y = myConfig.currentPos.y
+		  	  	  	    +  (myConfig.length1 * sin(myConfig.phi1 * PI/180) * sin(myConfig.phi0 * PI/180))
                         +  (myConfig.length2 * sin((myConfig.phi1 + myConfig.phi2) * PI/180)) * sin(myConfig.phi0 * PI/180)
                         +  (myConfig.length3 * sin((myConfig.phi1 + myConfig.phi2 + myConfig.phi3)) * PI/180) * sin(myConfig.phi0 * PI/180);
 
-  currentPosition.z = myConfig.currentPos.z +  (myConfig.length1 * sin(myConfig.phi1 * PI/180)
-                                  +  (myConfig.length2 * sin((myConfig.phi1 + myConfig.phi2) * PI/180))
+  currentPosition.z = myConfig.currentPos.z
+		                +  (myConfig.length1 * sin(myConfig.phi1 * PI/180)
+                        +  (myConfig.length2 * sin((myConfig.phi1 + myConfig.phi2) * PI/180))
                         +  (myConfig.length3 * sin((myConfig.phi1 + myConfig.phi2 + myConfig.phi3) * PI/180)));
   return currentPosition;
 }
