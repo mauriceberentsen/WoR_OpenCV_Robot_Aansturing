884c587e8e9b6f648b9117b61ba0a29a652f7786
Agit Tunc <A.Tunc1@student.han.nl>
2017-04-02 19:32:15 +0200

    Versie 1.3
    Moore–Penrose pseudoinverse dit keer correct toegepast. Programma  lijkt te werken. Bepaalde coordinaten vindt die niet, ik denk dat dit met de configuratie te maken heeft. Voor de x positie weet ik zeker dat dit niet groter mag zijn dan de lengte van de arm. Want anders kom je er toch niet bij. Dus limit voor x pos is : mag niet groter dan lengte van arm zijn. Voor y pos zal vast ook wel een uitspraak gemaakt kunnen worden. Ik zie coordinaat (8,0) tot (8,2) goed gaat. Maar (8,3) niet want dan blijft die maar zoeken.

--- src/kinematica/main.cpp
+++ src/kinematica/main.cpp
@@ -53,13 +53,13 @@ Matrix<2,3,double> calculateJacobiMatrix(Matrix<2,1,double> coordinates,Matrix<3
 {
 	 Matrix<2,3,double> result;
 
-  	 result.at(0).at(0)  = l1 * std::cos(angles[0][0] * PI / 180.0) + l2 * std::cos((angles[0][0] + angles[1][0]) * PI / 180.0) + l3 * std::cos((angles[0][0] + angles[1][0] + angles[2][0]) * PI / 180.0);
-  	 result.at(0).at(1)  = l2 * std::cos((angles[0][0] + angles[1][0])* PI / 180.0) + l3 * std::cos((angles[0][0] + angles[1][0] + angles[2][0])* PI / 180.0);
-  	 result.at(0).at(2)  = l3 * std::cos((angles[0][0] + angles[1][0] + angles[2][0])* PI / 180.0);
+  	 result.at(0).at(0)  = l1 * std::cos(angles.at(0).at(0) * PI / 180.0) + l2 * std::cos((angles.at(0).at(0) + angles.at(1).at(0)) * PI / 180.0) + l3 * std::cos((angles.at(0).at(0) + angles.at(1).at(0) + angles.at(2).at(0)) * PI / 180.0);
+  	 result.at(0).at(1)  = l2 * std::cos((angles.at(0).at(0) + angles.at(1).at(0))* PI / 180.0) + l3 * std::cos((angles.at(0).at(0) + angles.at(1).at(0) + angles.at(2).at(0))* PI / 180.0);
+  	 result.at(0).at(2)  = l3 * std::cos((angles.at(0).at(0) + angles.at(1).at(0) + angles.at(2).at(0))* PI / 180.0);
 
-  	 result.at(1).at(0)  = l1 * -std::sin(angles[0][0] * PI / 180.0) + l2 * -std::sin((angles[0][0] + angles[1][0]) * PI / 180.0) + l3 * -std::sin((angles[0][0] + angles[1][0] + angles[2][0]) * PI / 180.0);
-  	 result.at(1).at(1)  = l2 * -std::sin((angles[0][0] + angles[1][0])* PI / 180.0) + l3 * -std::sin((angles[0][0] + angles[1][0] + angles[2][0])* PI / 180.0);
-  	 result.at(1).at(2)  = l3 * -std::sin((angles[0][0] + angles[1][0] + angles[2][0])* PI / 180.0);
+  	 result.at(1).at(0)  = l1 * -std::sin(angles.at(0).at(0) * PI / 180.0) + l2 * -std::sin((angles.at(0).at(0) + angles.at(1).at(0)) * PI / 180.0) + l3 * -std::sin((angles.at(0).at(0) + angles.at(1).at(0) + angles.at(2).at(0)) * PI / 180.0);
+  	 result.at(1).at(1)  = l2 * -std::sin((angles.at(0).at(0) + angles.at(1).at(0))* PI / 180.0) + l3 * -std::sin((angles.at(0).at(0) + angles.at(1).at(0) + angles.at(2).at(0))* PI / 180.0);
+  	 result.at(1).at(2)  = l3 * -std::sin((angles.at(0).at(0) + angles.at(1).at(0) + angles.at(2).at(0))* PI / 180.0);
 
   	 return result;
 }
@@ -71,10 +71,12 @@ int main(int argc, char **argv)
 	double precision = 0.1;
 	Matrix<3,1,double> currentPose ({45,45,45});
 
-	auto result = forward_kinematic(x0,y0,l1,currentPose[0][0],l2,currentPose[1][0],l3,currentPose[2][0]);
+	auto result = forward_kinematic(x0,y0,l1,currentPose.at(0).at(0),l2,currentPose.at(1).at(0),l3,currentPose.at(2).at(0));
 
     Matrix<2,1,double> e({result.first,result.second});
-	Matrix<2,1,double> g({10,10});
+	Matrix<2,1,double> g({8.0,1.0});
+
+	if(g.at(0).at(0) > l1+l2+l3 ) throw std::runtime_error("x pos can never be reached because arm is not long enough");
 
 	std::cout<<e<<std::endl;
 	std::cout<<g<<std::endl;
@@ -83,14 +85,13 @@ int main(int argc, char **argv)
 
 	while(!e.approxEqual(g,precision))
 	{
-		std::cout<<i<<std::endl;
 		++i;
 		//stap1 compute jacobi matrix
 		Matrix<2,3,double> jacobiMatrix = calculateJacobiMatrix(e,currentPose); // resultaat van jacobijnse kom hier in. Jacobijn is afgeleide van forward_kinematic
 		//std::cout<<jacobiMatrix<<std::endl;
 
-		//stap2 using Moore–Penrose pseudoinverse  (A†=(AT * A)−1 * AT)
-		Matrix<3,2,double> jacobiInverse = (jacobiMatrix.transpose() * jacobiMatrix).inverse() * jacobiMatrix.transpose();
+		//stap2 using Moore–Penrose pseudoinverse  (A+=A*(AA*)-1)
+		Matrix<3,2,double> jacobiInverse =  jacobiMatrix.transpose() * (jacobiMatrix * jacobiMatrix.transpose()).inverse();
 		//std::cout<<jacobiInverse<<std::endl;
 
 		//stap3
@@ -105,11 +106,12 @@ int main(int argc, char **argv)
 		currentPose = currentPose + deltaPose;
 
 		//stap6 compute new e vector
-		auto coordinates= forward_kinematic(x0,y0,l1,currentPose[0][0],l2,currentPose[0][1],l3,currentPose[0][2]);
+		auto coordinates= forward_kinematic(x0,y0,l1,currentPose.at(0).at(0),l2,currentPose.at(1).at(0),l3,currentPose.at(2).at(0));
 
 		e = {coordinates.first,coordinates.second};
 
 	}
+	std::cout<<i<<std::endl;
 		return 0;
 }
 
