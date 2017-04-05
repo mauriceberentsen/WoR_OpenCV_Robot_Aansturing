4091a76743f1b9a7aa086c9b1913aeb61c0be8cb
Agit Tunc <A.Tunc1@student.han.nl>
2017-04-05 23:44:53 +0200

    Code cleanup
    double code weg gehaald. Veel staat nu in de beroeps product workspace. Kinematic node verwacht nu als argumenten begin hoek posities en coordinaten van doel.

--- src/wor-r_bp2_ws/src/robot_arm_aansturing/src/kinematics/main.cpp
+++ src/wor-r_bp2_ws/src/robot_arm_aansturing/src/kinematics/main.cpp
@@ -12,35 +12,52 @@
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "kinematic_node");
-	try
+
+	if (argc == 7)
 	{
+		try
+		{
 
-		//14.5 cm eerste arm shoulder servo to elbow sevo
-		//18.7 cm tweede arm elbow servo to wrist servo
-		//12   cm derde arm  wrist servo to uit einde van gripper
-			Kinematic robotArm({{-30,90},{0,135},{-90,90}},{14.5,18.7,12.0});
-		  	double x0 = 0.0;
-			double y0 = 0.0;
-			double beta = 0.1;
-			double precision = 0.1;
-			Matrix<3,1,double> currentPose (
-											{
-												{{45}},		//hoek van shoulder
-												{{45}},			//hoek van elbow
-												{{45}},			//hoek van wrist
-											}
-											);
-			Matrix<2,1,double> g({20.0,20.0});
+			//14.5 cm eerste arm shoulder servo to elbow sevo
+			//18.7 cm tweede arm elbow servo to wrist servo
+			//12   cm derde arm  wrist servo to uit einde van gripper
+				Kinematic robotArm({{-30,90},{0,135},{-90,90}},{14.5,18.7,12.0});
+				double x0 = 0.0;
+				double y0 = 0.0; //rekening houden met de hoogte van de base misschien?? 6.7 cm
+				double beta = 0.1;
+				double precision = 0.1;
 
-			robotArm.executeMotionPlanning(x0,y0,beta,precision,currentPose,g);
+				//Reminder Stand van de hoeken heeft wel effect op of doel haalbaar is
+				Matrix<3,1,double> currentPose (
+												{
+													{{atof(argv[2])}},			//hoek van shoulder
+													{{atof(argv[3])}},			//hoek van elbow
+													{{atof(argv[4])}}			//hoek van wrist
+												}
+												);
+				Matrix<2,1,double> g({atof(argv[5]),atof(argv[6])});
+
+				robotArm.executeMotionPlanning(x0,y0,beta,precision,currentPose,g);
 
 
+				//			auto schuine_zijde = std::sqrt(std::pow(20,2) + std::pow(20,2));
+				//			std::cout<<schuine_zijde<<std::endl;
+				//			auto getal = 20/schuine_zijde;
+				//			std::cout<<getal<<std::endl;
+				//			std::cout<<std::acos(getal) * 180 / PI<<std::endl;
+
+
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Exception: " << e.what() << std::endl;
+		}
 	}
-	catch (std::exception& e)
+	else
 	{
-		std::cout << "Exception: " << e.what() << std::endl;
+		ROS_INFO("usage: Base_angle shoulder_angle elbow_angle wrist_angle target_x target_y \n\r");
+		return 1;
 	}
-
 	return 0;
 }
 
